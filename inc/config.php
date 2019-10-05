0ca240f2cda67354b53fe267271238cecb67348f
Rigsto <rigsto@users.noreply.github.com>
2019-10-05 20:43:28 +0700

    added protection

--- inc/config.php
+++ inc/config.php
@@ -1,1 +1,18 @@
 <?php
+define('DB_NAME','auriga_refillwater');
+
+define('DB_USER','auriga_refill');
+
+define('DB_PASSWORD','RefillWater');
+
+define('DB_HOST', 'localhost');
+
+define('DB_CHARSET', 'utf8mb4');
+
+define('DB_COLLATE', '');
+
+$db = new mysqli("localhost", "auriga_refill", "RefillWater", "auriga_refillwater");
+
+if (!$db) {
+    die("Bad Server Connection");
+}
