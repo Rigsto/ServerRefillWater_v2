60af4f5172471141b66850926c52621027c2f043
Rigsto <rigsto@users.noreply.github.com>
2019-10-05 21:13:30 +0700

    login v1

--- login.php
+++ login.php
@@ -1,1 +1,27 @@
 <?php
+include('inc/config.php');
+
+//$email = $_POST['email'];
+//$pass = $_POST['password'];
+$email = $_GET['email'];
+$pass = $_GET['pass'];
+
+$query = $db->query("SELECT * FROM user WHERE (email = '$email') AND (password = '$pass')");
+$jum = mysqli_num_rows($query);
+$validate = mysqli_fetch_assoc($query);
+$response = array();
+
+if ($jum == 1) {
+    $response['code'] = 1;
+
+    $user = array();
+    $user["name"] = $validate["name"];
+    $user["api"] = $validate["spi"];
+
+    $response['profile'] = $user;
+
+    echo json_encode($response);
+} else {
+    $response['code'] = 0;
+    echo json_encode($response);
+}
