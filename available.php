afc92cf456790abd1cdd1752e2db9ccdf4cbfedc
Rigsto <rigsto@users.noreply.github.com>
2019-10-06 12:50:44 +0700

    available v3

--- available.php
+++ available.php
@@ -3,15 +3,15 @@
 include('inc/config.php');
 
 $query = $db->query("SELECT * FROM dispenser ORDER BY remain DESC");
-$response = array();
 
+$response['dispensers'] = array();
 while ($row = mysqli_fetch_assoc($query)) {
     $dis = array();
     $dis['place'] = $row['place'];
     $dis['floor'] = $row['floor'];
     $dis['remain'] = ($row['remain'] / $row['max']) * 100;
 
-    array_push($response, $dis);
+    array_push($response['dispensers'], $dis);
 }
 
 echo json_encode($response);
