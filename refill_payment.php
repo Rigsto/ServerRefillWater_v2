1e2d16283feb4c5386658267ebf6b855257c5b2c
Rigsto <rigsto@users.noreply.github.com>
2019-10-12 18:01:14 +0700

    payment v7

--- refill_payment.php
+++ refill_payment.php
@@ -7,21 +7,23 @@ $dispenser_id = $_GET['dis'];
 $price_code = $_GET['code'];
 
 $query = $db->query("SELECT * FROM user WHERE api='$api'");
-$id = mysqli_fetch_assoc($query)['id'];
-$money = mysqli_fetch_assoc($query)['money'];
+$x = mysqli_fetch_assoc($query);
+$id = $x['id'];
+$money = $x['money'];
 
 $refill = $db->query("SELECT * FROM refill_price WHERE id=$price_code");
-$size = mysqli_fetch_assoc($refill)['size'];
-$price = mysqli_fetch_assoc($refill)['price'];
+$y = mysqli_fetch_assoc($refill);
+$size = $y['size'];
+$price = $y['price'];
 
 $dispenser = $db->query("SELECT remain FROM dispenser WHERE id=$dispenser_id");
 $remain = mysqli_fetch_assoc($dispenser)['remain'];
-$updateremain = $db->query("UPDATE dispenser SET remain=($remain-$price) WHERE id=$dispenser_id");
+$updateremain = $db->query("UPDATE dispenser SET remain=($remain-$size) WHERE id=$dispenser_id");
 
 $timestamp = date("Y-m-d H:i:s");
-$history = $db->query("INSERT INTO `history` VALUES (NULL, $id, 'refill', $dispenser_id, NULL, $size, $timestamp)");
+$history = $db->query("INSERT INTO `history` VALUES (NULL, $id, 'refill', $dispenser_id, NULL, $size, NULL)");
 
-$user = $db->query("UPDATE `user` SET money=($money-$price) WHERE id=$id");
+$user = $db->query("UPDATE user SET money=($money-$price) WHERE id=$id");
 
 $response = array();
 if ($updateremain == 1 && $history == 1 && $user == 1) {
